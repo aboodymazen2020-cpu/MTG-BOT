526139e403dd2c5bb2f4522bc883ed053cfc8b81
ABO QAHT <aboodymazen2020@gmail.com>
2026-06-07 21:02:14 +0300

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// عداد الزوار
-if (!localStorage.getItem("mtg_visited")) {
-  let count = Number(localStorage.getItem("mtg_visits") || 0);
-  localStorage.setItem("mtg_visits", count + 1);
-  localStorage.setItem("mtg_visited", "yes");
-}
