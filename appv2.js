640370aacf928df54138606e518fa3dd9cb06bf0
ABO QAHT <aboodymazen2020@gmail.com>
2026-06-07 21:01:01 +0300

    Create appv2.js

---
+++ appv2.js
@@ -0,0 +1,5 @@
+function saveWebhooks() {
+  localStorage.setItem("https://canary.discord.com/api/webhooks/1513240622302236815/zNFKsUaXAh9L4Lkvlz_WvY7TG2R3OaRQlV-l1USwIveI1sIcYQpySDK3SMz_i8-AVbfL", assignWebhook.value);
+  localStorage.setItem("https://canary.discord.com/api/webhooks/1513240622302236815/zNFKsUaXAh9L4Lkvlz_WvY7TG2R3OaRQlV-l1USwIveI1sIcYQpySDK3SMz_i8-AVbfL", excuseWebhook.value);
+  alert("✅ تم حفظ إعدادات الديسكورد");
+}
